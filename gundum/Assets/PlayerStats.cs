260b74aabd9ef4d41763f185493d25b2e6468043
walth <walterh24@gmail.com>
2020-02-01 17:03:26 -0500

    working on damage, health, and currency
    - added game over and main menu screens

---
+++ gundum/Assets/PlayerStats.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField] int currentHealth = 100; //player can take 4 hits
+    [SerializeField] int maxHealth = 100; //player can take 4 hits
+    [SerializeField] int damageValue = 25;
+    [SerializeField] int scrap = 0;   //currency
+    [SerializeField] int missles = 1; //missle to load into weapon system. can only carry one
+
+    public bool Heal()
+    {
+        bool hasHealed = false;
+
+        if (currentHealth < maxHealth)
+        {
+            currentHealth += damageValue;
+            hasHealed = true;
+        }
+
+        return hasHealed;
+    }
+    public void TakeDamage()
+    { 
+        if(currentHealth > 0)
+            currentHealth -= damageValue;
+        else
+        {
+            Debug.Log("GAME OVER");
+
+        }
+
+    }
+    public void PickUpScrap(int amt) { scrap += amt; }
+    public void SpendScrap(int amt) { scrap -= amt; }
+
+    public void LoadMissle() 
+    {
+        if(missles == 1)
+            missles -= 1;
+    }
+
+    public void PickUpMissle()
+    {
+        if (missles == 0)
+            missles += 1;
+    }
+}
---
+++ gundum/Assets/ScapBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScapBox : MonoBehaviour
+{
+    [SerializeField] int scrapValue = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerStats>().PickUpScrap(scrapValue);
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetScrapValue(int amt) { scrapValue = amt; }
+}
