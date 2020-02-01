db03342d4fadcf5dbb3b3131d283a9111e9edc80
Hill <walter.hill@champlain.edu>
2020-01-31 19:48:01 -0500

    working on movement & input

---
+++ gundum/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 3, maxVelocity = 3;
+
+    Rigidbody2D rb;
+    float xMove;
+    Vector2 movement = Vector2.zero;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        CheckInput();
+        Move();
+    }
+
+    void Move()
+    {
+        if (xMove != 0)
+        {
+            movement = new Vector2(xMove * moveSpeed, 0);
+
+            if(Mathf.Abs(rb.velocity.x) < maxVelocity)
+                rb.AddForce(movement);
+        }
+    }
+
+    void CheckInput()
+    {
+        xMove = Input.GetAxis("LeftStickX");
+    }
+}
