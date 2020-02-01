87fd95b3afaba3ba769dd3a2fc088399b9879b17
walth <walterh24@gmail.com>
2020-02-01 11:27:52 -0500

    screen shake is noice n juicy

--- gundum/Assets/Scripts/FollowCamera.cs
+++ gundum/Assets/Scripts/FollowCamera.cs
@@ -4,78 +4,23 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
+    [Header("Camera Follow Values")]
     [SerializeField] Transform target;
-    [SerializeField] float smoothSpeed = 0.125f;
     [SerializeField] Vector3 offset = Vector3.zero;
-    [SerializeField] Camera shakyCamera;
-
-    private bool shouldFollow;
-    float camShakeValue = 0;
-
-    //TODO: screen shake
-    float shake;
-    float maxAngle = 10, maxOffset = 5;
-    Camera mainCam;
-        
-    private void Awake()
-    {
-        mainCam = GetComponent<Camera>();
-    }
+    [SerializeField] float smoothingPercentage = 0.05f;
 
     void FixedUpdate()
     {
+        CameraFollow();
+    }
+
+    void CameraFollow()
+    {
         Vector3 desiredPos = target.position + offset;
-        transform.position += AsymptoticAverageLerp(transform.position, desiredPos, .05f);
+        transform.position += AsymptoticAverageLerp(transform.position, desiredPos, smoothingPercentage);
         transform.LookAt(target);
 
-        if (Input.GetKeyDown(KeyCode.S))
-            AddTrauma(.5f);
-
-        CameraShake(camShakeValue);
-    }
-
-
-
-    //Camera shake
-    // - trauma level based on outside factor. goes down at a fixed, linear rate. Damage/stress adds trauma
-    // Camera shake is trauma^2 or ^3 
-    //Translational + rotational shake
-    // angle  = maxAngle * shake * GetRandomFloatNegOneToOne(); rotational
-    // xOffset = maxOffset * shake * GetRandomFloatNegOneToOne(); translational
-    // yOffset  = maxOffset * shake * GetRandomFloatNegOneToOne();
-
-    // shakyCamera.angle = camera.angle + angle;
-    // shakyCamera.center = camera.center + Vec2(offsetX, offsetY);
-
-    //use perlin noise GetPerlinNoies(seed, time,...)
-
-    public void AddTrauma(float intensity)
-    {
-        camShakeValue = intensity;
-    }
-
-    void CameraShake(float intensity)
-    {
-        if(intensity > 0)
-        {
-            shake = Random.Range(0.001f, 1.0f);
-            float angle = maxAngle * shake * Random.Range(-1, 1);
-            float xOffset = maxOffset * shake * Random.Range(-1, 1);
-            float yOffset = maxOffset * shake * Random.Range(-1, 1);
-
-            //Translational shake - currently doesn't work
-            Vector2 foo = mainCam.rect.center + new Vector2(xOffset, yOffset);
-            shakyCamera.rect.center.Set(foo.x, foo.y);
-            Debug.Log(shakyCamera.rect.center);
-            mainCam.rect = shakyCamera.rect;
-            
-            //Rotational shake
-            shakyCamera.transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, angle);
-            transform.localEulerAngles = shakyCamera.transform.localEulerAngles;
-            shakyCamera.transform.localEulerAngles = Vector3.zero;
-           
-            camShakeValue -= Time.fixedDeltaTime;
-        }
+        transform.rotation = Quaternion.identity;
     }
 
     Vector3 AsymptoticAverageLerp(Vector3 originPos, Vector3 targetPos, float percentage)
@@ -84,5 +29,4 @@ public class FollowCamera : MonoBehaviour
         //x += target-x * .1;
         return (targetPos - originPos) * percentage;
     }
-
 }
---
+++ gundum/Assets/ShakeCamera.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeCamera : MonoBehaviour
+{
+    [Header("Camera Shake Values")]
+    [SerializeField] Camera shakyCamera = null;
+    [SerializeField] float shakeFrequency = 15;
+    [SerializeField] float maxAngle = 3;
+    [SerializeField, Range(0.001f, 1)] float maxOffset = 1.5f;
+    [SerializeField] bool debugMode = false;
+
+    float camShakeValue = 0;
+    float shake, seed;
+
+    bool debugShake;
+
+    private void Awake()
+    {
+        seed = Random.value;
+    }
+ 
+    void FixedUpdate()
+    {
+        if (debugMode)
+        {
+            CheckInput();
+
+            if (debugShake)
+                AddTrauma(.25f, 1);
+        }
+
+        CameraShake();
+    }
+
+    //intensity must be between 0 and 1
+    public void AddTrauma(float duration, float intensityScale, float frequency = int.MinValue, float maxRotAngle = int.MinValue, float maxPosOffset = int.MinValue)
+    {
+        camShakeValue = duration;
+        
+        shake = intensityScale; //could normalize this
+
+        if (frequency != int.MinValue)
+            shakeFrequency = frequency;
+        if (maxRotAngle != int.MinValue)
+            maxAngle = maxRotAngle;
+        if (maxPosOffset != int.MinValue)
+            maxOffset = maxPosOffset;
+    }
+
+    void CameraShake()
+    {
+        if (camShakeValue > 0)
+        {
+            //Noise calculations
+            float angle = maxAngle * shake * (Mathf.PerlinNoise(seed, Time.time * shakeFrequency) * 2 - 1);
+            float xOffset = maxOffset * shake * (Mathf.PerlinNoise(seed + 1, Time.time * shakeFrequency) * 2 - 1);
+            float yOffset = maxOffset * shake * (Mathf.PerlinNoise(seed + 2, Time.time * shakeFrequency) * 2 - 1);
+
+            //Translational shake
+            shakyCamera.transform.localPosition = new Vector3(xOffset, yOffset, 0);
+            transform.localPosition = new Vector3(transform.localPosition.x + shakyCamera.transform.localPosition.x, 
+                                                  transform.localPosition.y + shakyCamera.transform.localPosition.y, 
+                                                  transform.localPosition.z);
+
+            //Rotational shake
+            shakyCamera.transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, angle);
+            transform.localEulerAngles = shakyCamera.transform.localEulerAngles;
+            
+            shakyCamera.transform.localEulerAngles = Vector3.zero;
+            shakyCamera.transform.position = transform.position;
+
+            camShakeValue -= Time.fixedDeltaTime;
+        }
+    }
+
+    void CheckInput()
+    {
+        debugShake = Input.GetKeyDown(KeyCode.S);
+    }
+}
