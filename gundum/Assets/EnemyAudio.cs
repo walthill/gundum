e54566a834e32b5e564a8e9f0db7a73fa42d15dd
walth <walterh24@gmail.com>
2020-02-01 19:07:43 -0500

    added hit audio to enemies. a little early project settings work

---
+++ gundum/Assets/EnemyAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAudio : MonoBehaviour
+{
+    [SerializeField]
+    List<AudioClip> hitSounds;
+    private List<AudioSource> sourceList;//single shot sound sources.
+
+    void Awake()
+    {
+        InitAudio();
+    }
+
+    void InitAudio()
+    {
+        sourceList = new List<AudioSource>();
+
+        foreach (AudioClip c in hitSounds)
+        {
+            AudioSource s = gameObject.AddComponent<AudioSource>();
+            s.clip = c;
+            sourceList.Add(s);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag.Contains("Player")) //Deal damage to player with enemy bullets
+        {
+            //play random grunt sound
+            int index = Random.Range(0, hitSounds.Count - 1);
+            sourceList[index].Play();
+        }
+    }
+}
